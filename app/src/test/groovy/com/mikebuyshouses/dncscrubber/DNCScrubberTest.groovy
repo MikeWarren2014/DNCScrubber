c3d2a68c14cb92b478b4c50e5d9943ab5d36e102
Mike Warren <mwarren04011990@gmail.com>
2023-09-26 04:26:25 -0400

    fixing broken test cases, as a result of the changes

--- app/src/test/groovy/com/mikebuyshouses/dncscrubber/DNCScrubberTest.groovy
+++ app/src/test/groovy/com/mikebuyshouses/dncscrubber/DNCScrubberTest.groovy
@@ -2,10 +2,12 @@ package com.mikebuyshouses.dncscrubber
 
 import com.mikebuyshouses.dncscrubber.datamanip.DNCScrubber
 import com.mikebuyshouses.dncscrubber.enums.PhoneTypes
+import com.mikebuyshouses.dncscrubber.models.AddressModel
 import com.mikebuyshouses.dncscrubber.models.BaseDataRowModel
 import com.mikebuyshouses.dncscrubber.models.BatchSkipTracingDataRowModel
 import com.mikebuyshouses.dncscrubber.models.PhoneModel
 import com.mikebuyshouses.dncscrubber.models.TestDataRowModel
+import org.apache.poi.ss.formula.functions.Address
 import spock.lang.Specification
 
 class DNCScrubberTest extends Specification {
@@ -15,7 +17,24 @@ class DNCScrubberTest extends Specification {
                 new BatchSkipTracingDataRowModel(
                         firstName: "First",
                         lastName: "TestUser",
-                        propertyAddress: "1234 Main Street",
+                        propertyAddressModel: new AddressModel(
+                                address: "1234 Main Street",
+                                city: "Indianapolis",
+                                state: "IN",
+                                zip: "46220",
+                        ),
+                        inputMailingAddressModel: new AddressModel(
+                                address: "1000 Main Street",
+                                city: "Indianapolis",
+                                state: "IN",
+                                zip: "46220",
+                        ),
+                        mailingAddressModel: new AddressModel(
+                                address: "1000 Main Street",
+                                city: "Indianapolis",
+                                state: "IN",
+                                zip: "46220",
+                        ),
                         childPhoneModels: [
                                 new PhoneModel(
                                         isDNC: false,
@@ -34,7 +53,24 @@ class DNCScrubberTest extends Specification {
                 new BatchSkipTracingDataRowModel(
                         firstName: "Empty",
                         lastName: "Record",
-                        propertyAddress: "1243 Main Street",
+                        propertyAddressModel: new AddressModel(
+                                address: "1243 Main Street",
+                                city: "Indianapolis",
+                                state: "IN",
+                                zip: "46220",
+                        ),
+                        inputMailingAddressModel: new AddressModel(
+                                address: "1001 Main Street",
+                                city: "Indianapolis",
+                                state: "IN",
+                                zip: "46220",
+                        ),
+                        mailingAddressModel: new AddressModel(
+                                address: "1001 Main Street",
+                                city: "Indianapolis",
+                                state: "IN",
+                                zip: "46220",
+                        ),
                         childPhoneModels: [],
                 ),
                 new BatchSkipTracingDataRowModel(
